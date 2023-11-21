1e54cc85261b015e0b78d3bbf8eee613f9a6cf11
Mohit <kmohit9111@gmail.com>
2023-11-22 00:13:11 +0530

    http requests

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,22 +7,29 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Schnitzel',
-      'A tasty Schnitzel - awesome',
-      'https://www.thespruceeats.com/thmb/BS5BdyQsGh5qQyRiuZMannmcxoY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-wiener-schnitzel-recipe-1447089-hero-02-18827aac4cbd4aec926350d4f9778e70.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Burger',
-      'A big fat burger',
-      'https://images.unsplash.com/photo-1571091718767-18b5b1457add?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D',
-      [new Ingredient('Bun', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  //   private recipes: Recipe[] = [
+  //     new Recipe(
+  //       'Schnitzel',
+  //       'A tasty Schnitzel - awesome',
+  //       'https://www.thespruceeats.com/thmb/BS5BdyQsGh5qQyRiuZMannmcxoY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-wiener-schnitzel-recipe-1447089-hero-02-18827aac4cbd4aec926350d4f9778e70.jpg',
+  //       [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //     ),
+  //     new Recipe(
+  //       'Burger',
+  //       'A big fat burger',
+  //       'https://images.unsplash.com/photo-1571091718767-18b5b1457add?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D',
+  //       [new Ingredient('Bun', 2), new Ingredient('Meat', 1)]
+  //     ),
+  //   ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice(); //slice because we want copy of this recipes array not the actual one becuase by default array and objects are reference types in js
---
+++ src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  ResolveFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Recipe } from './recipe.model';
+import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
+
+export const recipeResolver: ResolveFn<Recipe[]> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const recipes = inject(RecipeService).getRecipes();
+
+  if (recipes.length === 0) {
+    return inject(DataStorageService).fetchRecipes();
+  } else {
+    return recipes;
+  }
+};
